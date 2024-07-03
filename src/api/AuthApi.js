9c70160ae05e1301ff58aae52ef521bb639d53f2
Harsha Revanth Manchikanti <90206268+Harsha2306@users.noreply.github.com>
2024-07-03 23:19:57 +0530

    changed backend link

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://backend-olsx.onrender.com" }),
   endpoints: (builder) => ({
     postRegister: builder.mutation({
       query: ({ firstName, lastName, email, password }) => ({
